dd70b820447a2cf674b809d03c619609e623cfa8
1500371773@qq.com <1500371773@qq.com>
2024-08-07 23:03:33 +0800

    feat: 调整代码结构

--- src/components/Page/Page.stories.ts
+++ src/components/Page/Page.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { within, userEvent, expect } from '@storybook/test';
 
-import { Page } from './Page';
+import { Page } from 'a-ui-cl';
 
 const meta = {
   title: 'Example/Page',
--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Header } from './Header';
+import { Header } from 'a-ui-cl';
 import './page.css';
 
 type User = {
