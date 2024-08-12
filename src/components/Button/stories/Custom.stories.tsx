9bd96cedcd79a4e9216a6d1925f2d7fc61a1df2b
1500371773@qq.com <1500371773@qq.com>
2024-08-12 21:15:06 +0800

    feat: 添加token

--- src/components/Button/stories/Custom.stories.tsx
+++ src/components/Button/stories/Custom.stories.tsx
@@ -1,2 +1,30 @@
+import { Button } from 'a-ui-cl'
+import { makeStyles } from '@griffel/react'
+import { tokens } from '../../../tokens';
+
+const useClasses = makeStyles({
+  root: {
+    color: 'red',
+    backgroundColor: 'blue',
+    padding: '100px',
+    border: `10px solid ${tokens.colorBrandBackground}`
+  }
+})
+
+export const Custom = () => {
+  const styles = useClasses()
+
+  return (
+    <div
+      className={styles.root}
+    >
+      <Button
+        primary
+        label='Custom Button'
+      >
+      </Button>
+    </div>
+  )
+};
+
 export default {}
-export const Custom = () => <div>Custom</div>;
